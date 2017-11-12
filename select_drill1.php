62e7336a6a2c60a114914a3005b581ef6e2c45ec
Bouchat Marie-Ange <bouchat.marieange@gmail.com>
2017-11-12 13:00:23 +0100

    MySQL - SELECT - Drill 3 en cours

--- select_drill1.php
+++ select_drill1.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="mysql.css">
-    <title>MySQL</title>
+    <title>MySQL - SELECT - Drill1</title>
   </head>
 
   <body>
